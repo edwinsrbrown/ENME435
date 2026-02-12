931c0caad258f1363c56b244758df33808c6973f
edwinsrbrown <ebrown55@terpmail.umd.edu>
2026-02-12 10:14:35 -0500

    Update 20260210.py

--- 20260210.py
+++ 20260210.py
@@ -8,10 +8,10 @@ python -i 20260210.py --> makes the anaconda prompt itself interactive (e.g. can
 exit() --> used to leave a script, necessary for leaving interactive mode
 cls --> clears screen of previous commands on prompt
 '''
-print(' *')
-print(' * *')
-print(' *****')
-print('*  *')
-print('*   *')
+print('    *')
+print('   * *')
+print('  *****')
+print(' *     *')
+print('*       *')
 
 print("The answer is: ", (14*12)/(33*144-187))
