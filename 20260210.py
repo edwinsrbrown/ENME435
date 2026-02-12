95509576cc4b4c7ed6b3b5ab2f1f975e4e9502e6
edwinsrbrown <ebrown55@terpmail.umd.edu>
2026-02-12 10:13:11 -0500

    Update 20260210.py

--- 20260210.py
+++ 20260210.py
@@ -1,2 +1,17 @@
 A = 100
 print("The value of A is: ",A)
+
+'''
+python syntax:
+
+python -i 20260210.py --> makes the anaconda prompt itself interactive (e.g. can call variables and it will print values)
+exit() --> used to leave a script, necessary for leaving interactive mode
+cls --> clears screen of previous commands on prompt
+'''
+print(' *')
+print(' * *')
+print(' *****')
+print('*  *')
+print('*   *')
+
+print("The answer is: ", (14*12)/(33*144-187))
