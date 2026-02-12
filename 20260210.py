bbeb5987ef240f0dff08e0f226bdcd7969be3def
edwinsrbrown <ebrown55@terpmail.umd.edu>
2026-02-12 10:18:04 -0500

    Update 20260210.py

--- 20260210.py
+++ 20260210.py
@@ -1,10 +1,3 @@
-A = 100.1
-print("The value of A is: ",A)
-print(type(A))
-
-B = int(A)
-print(type(B))
-
 '''
 python syntax:
 
@@ -12,6 +5,14 @@ python -i 20260210.py --> makes the anaconda prompt itself interactive (e.g. can
 exit() --> used to leave a script, necessary for leaving interactive mode
 cls --> clears screen of previous commands on prompt
 '''
+
+A = 100.1
+print("The value of A is: ",A)
+print(type(A))
+
+B = int(A)
+print(type(B))
+
 print('    *')
 print('   * *')
 print('  *****')
