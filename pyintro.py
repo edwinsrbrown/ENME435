6e5dffd76db9f57f362f0972c6dca41b6fcfa85d
edwinsrbrown <ebrown55@terpmail.umd.edu>
2026-02-12 10:21:42 -0500

    Update and rename 20260210.py to pyintro.py

--- pyintro.py
+++ pyintro.py
@@ -1,9 +1,14 @@
 '''
-python syntax:
+command prompt syntax:
 
 python -i 20260210.py --> makes the anaconda prompt itself interactive (e.g. can call variables and it will print values)
 exit() --> used to leave a script, necessary for leaving interactive mode
 cls --> clears screen of previous commands on prompt
+
+python syntax:
+
+\t --> create a ta of space 
+\n --> move following info to a new line
 '''
 
 A = 100.1
@@ -20,3 +25,12 @@ print(' *     *')
 print('*       *')
 
 print("The answer is: ", (14*12)/(33*144-187))
+
+C = "World Lidar Day"
+print(C)
+
+D = "World Lidar \tDay"
+print(D)
+
+E = "World Lidar \nDay"
+print(E)
