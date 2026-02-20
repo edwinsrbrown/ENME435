c529d21fbbf46f8552dd64dc511ca1ad4d72d298
edwinsrbrown <ebrown55@terpmail.umd.edu>
2026-02-20 12:59:35 -0500

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -2,11 +2,28 @@
 import random as rand
 
 i = 0
-z = 0;
+z = 0
 while i < 10000:
-  i = i + 1
+  i += 1
   x = rand.randint(1,6)
   y = rand.randint(1,6)
-  if (x == y):
+  if(x == y):
     z = z + 1
 print("The percentage of rolls that are doubles are: ", z/i *100)
+
+# Q4
+import numpy as np
+
+tv1 = 0;
+tv2 = 0;
+w = input("Input value #1: ")
+x = input("Input value #2: ")
+y = input("Input value #3: ")
+z = input("Input value #4: ")
+
+
+if(w < x):
+  tv1 = w
+else if
+  
+  
