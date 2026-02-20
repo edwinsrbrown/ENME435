3433a1bf14f8b710248503a3f279c3edc97dc2b7
edwinsrbrown <ebrown55@terpmail.umd.edu>
2026-02-20 14:40:26 -0500

    Update HW1.py

--- HW1.py
+++ HW1.py
@@ -13,33 +13,25 @@ while i < 10000:
 print("The percentage of rolls that are doubles are: ", z/i *100)
 '''
 # Q4
-import numpy as np
-
-tv1 = 0;
-tv2 = 0;
 w = input("w = ")
 x = input("x = ")
 y = input("y = ")
 z = input("z = ")
 w, x, y, z = int(w), int(x), int(y), int(z)
 
-if(w < x & y < z):
-  tv1 = w
-  tv2 = y
+if(w < x and y < z):
+  w, y = y, w
   w, x, y, z = tv2, x, tv1, z
-if(w < x & y > z):
-  tv1 = w
-  tv2 = z
+elif(w < x and y > z):
+  w, z = z, w
   w, x, y, z = tv2, x, y, tv1
-if(w > x & y < z):
-  tv1 = x
-  tv2 = y
+elif(w > x and y < z):
+  x, y = y, x
   w, x, y, z = w, tv2, tv1, z
-if(w > x & y > z):
-  tv1 = x
-  tv2 = z
+elif(w > x and y > z):
+  x, z = z, x
   w, x, y, z = w, tv2, y, tv1
 
-print("After swap w,x,y,z = ",w,x,y,z)
+print("After swap w,x,y,z = ", w,x,y,z)
 
 
