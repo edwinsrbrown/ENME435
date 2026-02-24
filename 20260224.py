cecd808930f4f8affe4934c62db0a68bccdd1fd0
edwinsrbrown <ebrown55@terpmail.umd.edu>
2026-02-24 10:35:37 -0500

    Create 20260224.py

---
+++ 20260224.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+with open("20260224.txt") as f:
+  data = f.read()
+
+data = data.split('\n')
+
+print(data)
+
+x = [float(row.split('')[0]) for row in data]
+y = [float(row.split('')[1]) for row in data]
+
+plt.plot(x,y)
+
+plt.xlabel('X')
+plt.ylabel('Y')
+
+plt.title('Title Here')
+
+plt.show()
