0a3fd5ebdecdbbdaa217616627794f013e790fe8
edwinsrbrown <ebrown55@terpmail.umd.edu>
2026-02-26 10:08:40 -0500

    Update 20260226.py

--- 20260226.py
+++ 20260226.py
@@ -6,7 +6,10 @@ import imutils
 print("All packages imported properly!")
 
 image = cv2.imread("testudo.jpg")
-
 cv2.imshow("Caption here", image)
+
+# resizing image with imutls
+image_resized = imutils.resize(image, width=400)
+cv2.imshow("Resized Testudo", image_resized)
 
 cv2.waitKey(0) # so the image doesn't just appear and disappear
